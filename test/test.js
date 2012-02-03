e265d5f80462606fed931aa3305a4105850549e0
Tim Anema <timanema@gmail.com>
2012-02-02 20:42:23 -0500

    added a few tests and myself as a contributer

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var testHelper = require('./helpers.test.js');
+var express = require("express");
+var app = express.createServer();
+var helpers = require('../lib/express-helpers')(app);
+for (test in testHelper) {
+	testHelper[test]();
+}
+console.log("all tests completed successfully!");
+
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+app.set('view options', {layout: false});
+
+app.get('/', function(req, res){
+    res.render('test');
+});
+app.get('/test', function(req, res){
+    res.render('test');
+});
+
+app.listen(3000);
+console.log("testing node started on port 3000");
