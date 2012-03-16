6106cae850c8b53a5561c0dfea1c5d70e5498c00
Tim Anema <timanema@gmail.com>
2012-03-16 08:48:00 -0400

    working on documentation and making method usage more ituitive

--- docs/outputdoc.js
+++ docs/outputdoc.js
@@ -1,15 +1,18 @@
 var helpers = require('../lib/express-helpers.js')();
-var generate_docs = false,
-	generate_tests = true;
+var generate_docs = true,
+	generate_tests = false;
 var date = new Date(2007,10,20,1,1,1,1);
 var date_string = JSON.stringify(date);
 var choices = [ {value: 1,text:'First Choice' },{value: 2,text: 'Second Choice'},{value: '3',text: 'Third Choice'}];
+var exec = require("child_process").exec;
+var fs = require('fs');
 
 var methods = [
 	{helper: "button_to", args: ["google", "http://www.google.com"]},
 	{helper: "cdata", args: ['this is cdata']},
 	{helper: "checkbox_tag", args: ['user_check_box', {value:'user'}]},
 	{helper: "color_field_tag", args: ['color', {value: 5}]},
+	{helper: "css_tag", args: ['/stylesheet/style.css']},
 	{helper: "css_tag", args: ['/stylesheet/style.css']},
 	{helper: "date_tag", args: ['Installation[date]', {value: date}]},
 	{helper: "date_time_tag", args: ['Installation[datetime]', {value: date}]},
@@ -50,17 +53,26 @@ if(generate_tests){
 	console.log("var assert = require('assert'),helpers = require('../lib/express-helpers.js')();");
 	console.log(" ");
 }
+if(generate_docs){
+	exec("mkdir wiki");
+}
+
 for(var i = 0; i < methods.length; i++){
 	
 	var json = JSON.stringify(methods[i].args)
 	if(generate_docs){
-		console.log(" ")
-		console.log(methods[i].helper+"("+ json.substring(1, json.length - 1)+")")
-		console.log(" ");
-		console.log(" => ");
-		console.log(" ");
-		console.log(helpers[methods[i].helper].apply(this, methods[i].args))
-		console.log(" ");
+		var text = "\n";
+		text += methods[i].helper+"("+ json.substring(1, json.length - 1)+")\n \n";
+		text += " => \n \n";
+		text += helpers[methods[i].helper].apply(this, methods[i].args) + "\n";
+
+		fs.open("./wiki/" + methods[i].helper + ".txt", 'a', undefined, function(err, fd) { 
+            if(err) throw err; 
+            fs.write(fd, text, undefined, undefined, function(err, written) { 
+                 if(err) throw err; 
+                 fs.close(fd); 
+             }); 
+        }); 
 	}
 	
 	if(generate_tests){
