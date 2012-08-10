c8d92586a55d07e094c3b21449bcf31f16079858
Tim Anema <tim@zenapsis.com>
2012-08-10 16:21:52 -0400

    make tests backward compatible

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var testHelper = require('./helpers.test.js');
 console.log("all tests completed successfully!");
 
 var express = require("express");
-var app = express();
+var app = express.createServer ? express.createServer() : express();
 var helpers = require('../lib/express-helpers')(app);
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
