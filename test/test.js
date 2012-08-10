0ba287d8b9205d83c29916480f89c0af9a5eebc1
Tim Anema <tim@zenapsis.com>
2012-08-10 16:14:12 -0400

    added support for express 3.x

--- test/test.js
+++ test/test.js
@@ -2,13 +2,16 @@ var testHelper = require('./helpers.test.js');
 console.log("all tests completed successfully!");
 
 var express = require("express");
-var app = express.createServer();
+var app = express();
 var helpers = require('../lib/express-helpers')(app);
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
 app.set('view options', {layout: false});
 
 app.get('/', function(req, res){
+    res.render('test', {user: {id: 5, username: 'tim', name: 'Tim'}});
+});
+app.get('/test', function(req, res){
     res.render('test', {user: {id: 5, username: 'tim', name: 'Tim'}});
 });
 
